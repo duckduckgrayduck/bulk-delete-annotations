6d7d203475a71d91fcddedf549bbe2e871a39801
duckduckgrayduck <102841251+duckduckgrayduck@users.noreply.github.com>
2022-12-09 14:13:25 -0600

    Update main.py

--- main.py
+++ main.py
@@ -10,29 +10,15 @@ DocumentCloud using the standard API
 from documentcloud.addon import AddOn
 
 
-class HelloWorld(AddOn):
-    """An example Add-On for DocumentCloud."""
+class MassDeleteAnnotations(AddOn):
+    """Add-On that runs through all documents in a selection or query and deletes annotations for them""" 
 
     def main(self):
         """The main add-on functionality goes here."""
-        # fetch your add-on specific data
-        name = self.data.get("name", "world")
-
-        self.set_message("Hello World start!")
-
-        # add a hello note to the first page of each selected document
+   
         for document in self.get_documents():
-            # get_documents will iterate through all documents efficiently,
-            # either selected or by query, dependeing on which is passed in
-            document.annotations.create(f"Hello {name}!", 0)
-
-        with open("hello.txt", "w+") as file_:
-            file_.write("Hello world!")
-            self.upload_file(file_)
-
-        self.set_message("Hello World end!")
-        self.send_mail("Hello World!", "We finished!")
-
+            for note in doc_obj.annotations:
+                document.annotations.delete(note)
 
 if __name__ == "__main__":
-    HelloWorld().main()
+    MassDeleteAnnotations().main()
