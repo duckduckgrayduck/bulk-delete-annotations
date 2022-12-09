00d829e23edc09151125db6839477081120d6260
duckduckgrayduck <102841251+duckduckgrayduck@users.noreply.github.com>
2022-12-09 14:14:16 -0600

    Update main.py

--- main.py
+++ main.py
@@ -4,13 +4,11 @@ This is an Add-On derived from the Hello World Template, it allows you to mass d
 
 from documentcloud.addon import AddOn
 
-
 class MassDeleteAnnotations(AddOn):
     """Add-On that runs through all documents in a selection or query and deletes annotations for them""" 
 
     def main(self):
         """The main add-on functionality goes here."""
-   
         for document in self.get_documents():
             for note in doc_obj.annotations:
                 document.annotations.delete(note)
