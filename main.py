8b76473b4fde6976f573a3c47f337c9721e86e2c
duckduckgrayduck <102841251+duckduckgrayduck@users.noreply.github.com>
2023-02-21 10:39:26 -0600

    Update main.py

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ class MassDeleteAnnotations(AddOn):
         """The main add-on functionality goes here."""
         for document in self.get_documents():
             for note in document.annotations:
-                document.delete(note)
+                print(note)
 
 if __name__ == "__main__":
     MassDeleteAnnotations().main()
